038c809a579b89ab02861c978453436e9ad5380e
tgthuan2000 <tgthuan2000@gmail.com>
2022-07-04 18:01:07 +0700

    get data method, transaction recent

--- src/page/spending/components/Method.tsx
+++ src/page/spending/components/Method.tsx
@@ -1,21 +1,89 @@
 import { Link } from 'react-router-dom'
 import { methodItems } from '~/constant/spending'
+import NumberFormat from 'react-number-format'
+import { useEffect, useState } from 'react'
+import useAuth from '~/store/auth'
+import { client } from '~/sanityConfig'
+import clsx from 'clsx'
+
+interface Data {
+    _id: string
+    name: string
+    cost: number
+    receive: number
+}
+interface DataSanity {
+    _id: string
+    name: string
+    cost: number[]
+    receive: number[]
+}
 
 const Method = () => {
-    return (
-        <ul role='list' className='divide-y divide-gray-300'>
-            {methodItems.map((item) => (
-                <li key={item.id}>
-                    <Link to={`method/${item.id}`} className='px-4 py-4 flex hover:bg-gray-100 cursor-pointer'>
-                        <div className='w-2/3 truncate'>
-                            {item.type && <h4 className='font-medium'>{item.type}</h4>}
-                        </div>
-                        <div className='w-1/3 truncate text-right'>{item.amount && <h4>{item.amount}</h4>}</div>
-                    </Link>
-                </li>
-            ))}
-        </ul>
-    )
+    const [data, setData] = useState<Data[]>([])
+    const { userProfile } = useAuth()
+
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                const res: DataSanity[] = await client.fetch(
+                    `
+                    *[_type == "methodSpending" && user._ref == '${userProfile?._id}']
+                    {
+                        _id,
+                        name,
+                        "cost":  *[_type == "spending" && user._ref == '${userProfile?._id}' && method._ref == ^._id && kind._ref == "960d7f6f-8e80-48e2-a895-ff226b46e1bb"].amount,
+                        "receive":  *[_type == "spending" && user._ref == '${userProfile?._id}' && method._ref == ^._id && kind._ref == "6dd1d83c-34fa-401a-a569-ac4502b3d192"].amount
+                    }
+                `
+                )
+
+                setData(
+                    res.map(({ cost, receive, ...data }) => {
+                        return {
+                            ...data,
+                            cost: cost.reduce((a, b) => a + b, 0),
+                            receive: receive.reduce((a, b) => a + b, 0),
+                        }
+                    })
+                )
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        getData()
+    }, [])
+    if (data && data.length) {
+        return (
+            <ul role='list' className='divide-y divide-gray-300'>
+                {data.map((item) => (
+                    <li key={item._id}>
+                        <Link to={`method/${item._id}`} className='px-4 py-4 flex hover:bg-gray-100 cursor-pointer'>
+                            <div className='w-2/3 truncate'>
+                                {item.name && <h4 className='font-medium'>{item.name}</h4>}
+                            </div>
+                            <div className='w-1/3 truncate text-right'>
+                                {(item.cost || item.receive) && (
+                                    <h4>
+                                        <NumberFormat
+                                            value={item.receive - item.cost}
+                                            displayType='text'
+                                            thousandSeparator
+                                            className={clsx(
+                                                item.receive - item.cost < 0 ? 'text-red-500' : 'text-green-500',
+                                                'font-bold'
+                                            )}
+                                        />
+                                    </h4>
+                                )}
+                            </div>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        )
+    }
+    return null
 }
 
 export default Method
--- src/page/spending/components/Recent.tsx
+++ src/page/spending/components/Recent.tsx
@@ -1,26 +1,104 @@
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import { recentItems } from '~/constant/spending'
+import { client } from '~/sanityConfig'
+import moment from 'moment'
+import NumberFormat from 'react-number-format'
+import useAuth from '~/store/auth'
+import clsx from 'clsx'
+
+interface Data {
+    _id: string
+    _createdAt: string
+    name: string
+    category: {
+        _id: string
+        name: string
+    }
+    method: {
+        _id: string
+        name: string
+    }
+    kind: {
+        _id: string
+        name: string
+        style: string
+    }
+    description: string
+    amount: number
+    date: string
+}
 
 const Recent = () => {
-    return (
-        <ul role='list' className='divide-y divide-gray-300'>
-            {recentItems.map((item) => (
-                <li key={item.id}>
-                    <Link to={`transaction/${item.id}`} className='px-4 py-4 flex hover:bg-gray-100 cursor-pointer'>
-                        <div className='w-2/3 truncate'>
-                            {item.date && <span>{item.date}</span>}
-                            {item.type && <h4 className='font-bold'>{item.type}</h4>}
-                            {item.note && <span className='truncate'>{item.note}</span>}
-                        </div>
-                        <div className='w-1/3 truncate text-right'>
-                            {item.kind && <span className='font-medium'>{item.kind}</span>}
-                            {item.amount && <h4>{item.amount}</h4>}
-                        </div>
-                    </Link>
-                </li>
-            ))}
-        </ul>
-    )
+    const [data, setData] = useState<Data[]>([])
+    const { userProfile } = useAuth()
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                const res =
+                    await client.fetch(`*[_type == "spending" && user._ref == '${userProfile?._id}'] | order(_createdAt desc)[0...5]
+                {
+                    _id,
+                    _createdAt,
+                    name,
+                    category-> {
+                        _id,
+                        name
+                    },
+                    method-> {
+                        _id,
+                        name
+                    },
+                    kind-> {
+                        _id,
+                        name,
+                        style
+                    },
+                    description,
+                    amount,
+                    date
+                }`)
+
+                setData(res)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        getData()
+    }, [])
+
+    if (data && data.length) {
+        return (
+            <ul role='list' className='divide-y divide-gray-300'>
+                {data.map((item) => (
+                    <li key={item._id}>
+                        <Link
+                            to={`transaction/${item._id}`}
+                            className='px-4 py-4 flex hover:bg-gray-100 cursor-pointer'
+                        >
+                            <div className='w-2/3 truncate'>
+                                {item.date && <span>{moment(item.date).format('DD/MM/YYYY - HH:mm:ss')}</span>}
+                                {item.method && <h3 className='font-medium'>{item.method.name}</h3>}
+                                {item.description && (
+                                    <span className='truncate' title={item.description}>
+                                        {item.description}
+                                    </span>
+                                )}
+                            </div>
+                            <div className='w-1/3 truncate text-right'>
+                                {item.kind && <h4 className={clsx('font-bold')}>{item.kind.name}</h4>}
+                                {item.amount && (
+                                    <h4>
+                                        <NumberFormat value={item.amount} displayType='text' thousandSeparator />
+                                    </h4>
+                                )}
+                            </div>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        )
+    }
+    return <div className='py-3 text-center text-md animate-pulse'>Loading...</div>
 }
 
 export default Recent
