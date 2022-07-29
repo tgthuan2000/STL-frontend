95539955fa8366770b9163a4bec7fd864e0ec054
tgthuan2000 <tgthuan2000@gmail.com>
2022-07-29 23:59:49 +0700

    commit

--- src/page/spending/transaction-tab-table.tsx
+++ src/page/spending/transaction-tab-table.tsx
@@ -28,41 +28,47 @@ const TransactionTabTable = ({ query, params = {} }: TransactionTabTableProps) =
     }, [])
 
     if (recent.loading)
-        return Array.from(Array(5)).map((item, index, data) => (
-            <Fragment key={index}>
-                <tr className=' animate-pulse'>
-                    <td className='py-4 px-2'>
-                        <span className='flex flex-col gap-2 w-full'>
-                            <span className='block bg-gray-200 h-4 w-3/4 rounded-full' />
-                            <span className='block bg-gray-200 h-4 w-1/2 rounded-full' />
-                        </span>
-                    </td>
-                    <td className='py-4 px-2'>
-                        <span className='flex flex-col gap-2 w-full'>
-                            <span className='block mx-auto bg-gray-200 h-4 w-1/2 rounded-full' />
-                        </span>
-                    </td>
-                    <td className='py-4 px-2'>
-                        <span className='flex flex-col gap-2 w-full'>
-                            <span className='block mx-auto bg-gray-200 h-4 w-1/2 rounded-full' />
-                        </span>
-                    </td>
-                    <td className='py-4 px-2'>
-                        <span className='flex flex-col gap-2 w-full'>
-                            <span className='block mx-auto bg-gray-200 h-4 w-1/2 rounded-full' />
-                        </span>
-                    </td>
-                </tr>
-                <tr className='animate-pulse'>
-                    <td
-                        colSpan={4}
-                        className={clsx('pb-4 px-2', { 'border-b border-gray-200': data && index !== data.length - 1 })}
-                    >
-                        <span className='block bg-gray-200 h-4 w-4/5 rounded-full' />
-                    </td>
-                </tr>
-            </Fragment>
-        ))
+        return (
+            <>
+                {Array.from(Array(5)).map((item, index, data) => (
+                    <Fragment key={index}>
+                        <tr className=' animate-pulse'>
+                            <td className='py-4 px-2'>
+                                <span className='flex flex-col gap-2 w-full'>
+                                    <span className='block bg-gray-200 h-4 w-3/4 rounded-full' />
+                                    <span className='block bg-gray-200 h-4 w-1/2 rounded-full' />
+                                </span>
+                            </td>
+                            <td className='py-4 px-2'>
+                                <span className='flex flex-col gap-2 w-full'>
+                                    <span className='block mx-auto bg-gray-200 h-4 w-1/2 rounded-full' />
+                                </span>
+                            </td>
+                            <td className='py-4 px-2'>
+                                <span className='flex flex-col gap-2 w-full'>
+                                    <span className='block mx-auto bg-gray-200 h-4 w-1/2 rounded-full' />
+                                </span>
+                            </td>
+                            <td className='py-4 px-2'>
+                                <span className='flex flex-col gap-2 w-full'>
+                                    <span className='block mx-auto bg-gray-200 h-4 w-1/2 rounded-full' />
+                                </span>
+                            </td>
+                        </tr>
+                        <tr className='animate-pulse'>
+                            <td
+                                colSpan={4}
+                                className={clsx('pb-4 px-2', {
+                                    'border-b border-gray-200': data && index !== data.length - 1,
+                                })}
+                            >
+                                <span className='block bg-gray-200 h-4 w-4/5 rounded-full' />
+                            </td>
+                        </tr>
+                    </Fragment>
+                ))}
+            </>
+        )
 
     if (!recent.data || _.isEmpty(recent.data))
         return (
