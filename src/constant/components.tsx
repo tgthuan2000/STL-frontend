937a7eb2deb1d2800d5acfc55e38751386471493
tgthuan2000 <tgthuan2000@gmail.com>
2022-07-30 13:49:51 +0700

    commit

--- src/constant/components.tsx
+++ src/constant/components.tsx
@@ -260,3 +260,16 @@ export const menuLoanPages: IMenuBtn[] = [
     //     to: 'method',
     // },
 ]
+
+export const people = [
+    { id: 1, name: 'Wade Cooper' },
+    { id: 2, name: 'Arlene Mccoy' },
+    { id: 3, name: 'Devon Webb' },
+    { id: 4, name: 'Tom Cook' },
+    { id: 5, name: 'Tanya Fox' },
+    { id: 6, name: 'Hellen Schmidt' },
+    { id: 7, name: 'Caroline Schultz' },
+    { id: 8, name: 'Mason Heaney' },
+    { id: 9, name: 'Claudie Smitham' },
+    { id: 10, name: 'Emil Schaefer' },
+]
