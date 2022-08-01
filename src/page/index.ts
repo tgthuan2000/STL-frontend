ef94083ebba9ef88019033723fd5a50e3bf4e3b8
tgthuan2000 <tgthuan2000@gmail.com>
2022-08-01 21:52:54 +0700

    commit

--- src/page/index.ts
+++ src/page/index.ts
@@ -1,4 +1,4 @@
 export { default as Spending } from './spending'
-export { default as Loan } from './loan'
+export { default as Loan } from './loans'
 export { default as TimeKeeping } from './TimeKeeping'
 export { default as Auth } from './auth'
