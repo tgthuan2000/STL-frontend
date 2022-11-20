9d76ba09c767709f9a06f65c9aa0ed1c94b28c89
ThuanTran <tgthuan2000@gmail.com>
2022-11-20 16:15:33 +0700

    add filter to param url

--- src/features/spending/pages/TransactionTabTable.tsx
+++ src/features/spending/pages/TransactionTabTable.tsx
@@ -318,7 +318,7 @@ const EmptyTransactionTable = () => {
     return (
         <tr>
             <td colSpan={4} className='whitespace-nowrap py-4 px-2'>
-                <span className='block truncate w-full text-center text-lg text-gray-700 font-base'>
+                <span className='block truncate w-full text-center text-md text-gray-700 font-base'>
                     {TEMPLATE.EMPTY_DATA}
                 </span>
             </td>
